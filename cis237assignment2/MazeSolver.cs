651c297bc5185449bd3cde541e8e3d7a7eaed050
Gabru678 <PaulBath7@gmail.com>
2016-02-15 00:25:12 -0500

    made a bunch of logic decisions complete, print array is the only hting that needs work

--- cis237assignment2/MazeSolver.cs
+++ cis237assignment2/MazeSolver.cs
@@ -46,13 +46,17 @@ namespace cis237assignment2
             this.yStart = yStart;
 
             //Do work needed to use mazeTraversal recursive call and solve the maze.
+            Console.WriteLine("Hello, we will begin traversing the maze!");
 
-            //basically I would first want to check if there is a '#' on the left, if not change pointer to there.
+            mazeTraversal(xStart, yStart);
+            // Then Print out the solved maze ( if not done with the recursive method
+            // Ask Teacher if we would print out the maze EVERY iteration?
+            mazePrinter(this.maze);
 
-            if (xStart - 1 == '#')
-            {
-                // set new xstart to the new one
-            }
+            // Call the UI class again to ask what to do
+            // Or just print it out
+            Console.WriteLine("Should this be a UI \n Would you like to press a button to exit?");
+
         }
 
 
@@ -61,9 +65,69 @@ namespace cis237assignment2
         /// Feel free to change the return type if you like, or pass in parameters that you might need.
         /// This is only a very small starting point.
         /// </summary>
-        private void mazeTraversal()
+        private void mazeTraversal(int x, int y)
         {
             //Implement maze traversal recursive call
+
+            // Establish the maze and where we are beginning as a point
+            char current = this.maze[x, y];
+            
+            // Now do a bunch of cases on what would happen
+
+            //First I should check if where I'm at is valid
+            // otherwise I should drop an O
+            // Assuming its not already a # (wall)
+            if (current == '.')
+            {
+                this.maze[x, y] = 'O';
+            }
+            //If its a wall or an O we would exit the loop
+            if (current == '#' || current == 'O')
+            {
+                //How to exit recursive loop?
+                exit;
+            }
+
+            //basically I would first want to check if there is a '#' on the left, if not change pointer to there.
+            if (current != '#' || current != 'O')
+            {
+                // And shouldnt I make current = 'O' before I exit the loop
+                // by declaring it here?
+                mazeTraversal(x - 1, y);
+            }
+
+                // How should I be calling the multiple ones?
+            else if (current == '#' || current == 'O')
+            {
+                // this one is if not left move forward
+                // How would I check if the forward one isnt a #
+                // Isnt that the one handled by the default in the recursive call?
+                mazeTraversal(x, y + 1);
+            }
+
+            else if (current == '#' || current == 'O')
+            {
+                // This one is to move right
+                // Assuming the previous idea of recalling the correct method works
+                mazeTraversal(x + 1, y);
+            }
+
+            else if (current == '#' || current == 'O')
+            {
+                // This one is to move back
+                // This would be a last resort
+                mazeTraversal(x, y - 1);
+            }
+            
+            // This would be if we knew we were done
+            if (current != '#' || current != 'O' || current != '.')
+            {
+                // This one woudl be assuiming there are no moves left
+                // Would use the damn printArray method to print it out
+                // and tell the user that we are done
+                Console.WriteLine(" Congrats! \n The maze is completed and your path is below")
+            }
+        //This is the end of the Maze recursive call to traverse the maze
         }
     }
 }
--- cis237assignment2/mazePrinter.cs
+++ cis237assignment2/mazePrinter.cs
@@ -11,5 +11,36 @@ namespace cis237assignment2
 
         // seperate method to print a maze
         // Maybe add a method to print either the regular maze or the transposed one.
+        // public toString( char Array[] input){
+        //    return input;
+        //}
+
+        //public void outItGoes(char[,] maze)
+        //{
+        //    maze.ToString().ForEach(i => Console.WriteLine(i.ToString()));            
+        //}
+
+        //public override string ToString(char[,] maze)
+        //{
+            //foreach(var item in maze)
+            //{
+            //    Console.Write(maze.ToString);
+            //}
+        //}
+
+        //long[,] arr = new long[5, 4] { { 1, 2, 3, 4 }, { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 }, { 4, 4, 4, 4 } };
+
+        //int rowLength = arr.tLength(0);
+        //int colLength = arr.GetLength(1);
+
+        //for (int i = 0; i < rowLength; i++)
+        //{
+        //    for (int j = 0; j < colLength; j++)
+        //    {
+        //        Console.Write(string.Format("{0} ", arr[i, j]));
+        //    }
+        //    Console.Write(Environment.NewLine + Environment.NewLine);
+        //}
+        //Console.ReadLine();
     }
 }
